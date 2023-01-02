211a6a565d4d9ade123e453d51babc34fb96921a
Shisun <worlddominator007@gmail.com>
2023-01-02 10:47:17 +0700

    Finish #14

--- src/helpers/match.js
+++ src/helpers/match.js
@@ -9,6 +9,29 @@ export const hasDuplicates = list => {
 }
 
 export const matchPeople = people => {
+  let secretSantaList = []
+  let peopleList = [...people]
+  peopleList = peopleList.sort(() => 0.5 - Math.random());
+  people.forEach(person => {
+    const receiver = peopleList.find(p => p.email !== person.email);
+    if (receiver) {
+      peopleList = peopleList.filter(p => p.email !== receiver.email)
+      secretSantaList.push({
+        giver: person,
+        receiver,
+      });
+    } else {
+      const lastReceiver = secretSantaList[0];
+      const newReceiverIndex = secretSantaList.findIndex(receiver => {
+        return receiver.email !== lastReceiver.receiver.email;
+      })
+      secretSantaList.push({
+        giver: person,
+        receiver: secretSantaList[newReceiverIndex].receiver,
+      })
+      secretSantaList[newReceiverIndex].receiver = person;
+    }
+  })
   // Return an array of randomly matched up people.
   // The array should have objects with the following structure:
   // { giver: {name: 'NAME_HERE, email: 'EMAIL_HERE'}, receiver: {name: 'NAME_HERE, email: 'EMAIL_HERE'}}
@@ -17,6 +40,5 @@ export const matchPeople = people => {
   // 1. For every person there has to be a giver
   // 2. A person cannot be their own giver
 
-  let secretSantaList = []
   return secretSantaList
 }
